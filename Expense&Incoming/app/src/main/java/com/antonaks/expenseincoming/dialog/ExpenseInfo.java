9a2dab57ecb18757e2ba29ffe28e928be0599965
AntonAks <antonaks84@gmail.com>
2015-08-26 12:00:01 +0300

    Revert "add AlertDialog"
    This reverts commit ab7ac6f8a968e86d220c925a1dccc12b13805d4d.

--- Expense&Incoming/app/src/main/java/com/antonaks/expenseincoming/dialog/ExpenseInfo.java
+++ Expense&Incoming/app/src/main/java/com/antonaks/expenseincoming/dialog/ExpenseInfo.java
@@ -1,26 +1,7 @@
 package com.antonaks.expenseincoming.dialog;
 
-import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.os.Build;
-import android.os.Bundle;
-
-import com.antonaks.expenseincoming.R;
-
 /**
  * Created by AntonAks on 19.08.2015.
  */
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class ExpenseInfo extends DialogFragment {
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_info);
-
-        return builder.create();
-    }
+public class ExpenseInfo {
 }
